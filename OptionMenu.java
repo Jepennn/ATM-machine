b5cc5f711f12d52d1b6cb293d23cfa7486628e2d
Jepennn <jeppehesselgren@gmail.com>
2023-12-24 13:57:05 +0100

    Started to set upp to get login method

--- OptionMenu.java
+++ OptionMenu.java
@@ -1,3 +1,22 @@
-public class OptionMenu {
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.text.DecimalFormat;
+import java.util.HashMap;
 
+
+public class OptionMenu extends Account {
+    Scanner menuInput = new Scanner(System.in);
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0,00");
+
+    HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
+
+    //Validates the login information
+    public void getLogin()
+    {
+
+        data.put(123456789, 1234);
+        data.put(987654321, 9876);
+    }
 }
+
