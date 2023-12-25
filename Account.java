033078854525d0784bce1f4f42d8f1219223d604
Jepennn <jeppehesselgren@gmail.com>
2023-12-26 00:46:42 +0100

    Starting to set up funktions in Account class.

--- Account.java
+++ Account.java
@@ -1,3 +1,62 @@
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
 public class Account {
 
+    private int customerNumber;
+    private int pinCode;
+    private double savingBalance;
+    private double checkingBalance;
+
+    //Setting up Costumer number
+    public void SetCustomer(int customerNumber)
+    {
+        this.customerNumber = customerNumber;
+    }
+    //Returns the customernumber
+    public int getCustomerNum()
+    {
+        return customerNumber; 
+    }
+    //Setting up the PIN-code.
+    public void SetPinCode(int pinCode)
+    {
+        this.pinCode = pinCode;
+    }
+    //Returns the Pin
+    public int getPin()
+    {
+        return pinCode;
+    }
+
+    //Methods to handel saving account
+    
+    //set saving account balance
+    public void setSavingaccountbalance(double amount)
+    {
+        savingBalance = amount;
+    }
+
+    public void withDrawSavingAccount(double amount) throws IOException
+    {
+        if(amount > savingBalance)
+        {
+            throw new IOException("Not enough money, negative balance is not possible");
+        }
+        else
+        {
+            savingBalance = savingBalance - amount;
+            System.out.println(amount + " is now withdrawn" + "\n");
+            System.out.println("Your current balance left on the account is" + savingBalance);
+        }
+    }
+
+    public void withDrawCheckingAccount(double amount) throws IOException
+    {
+        if(amount > checkingBalance){
+            throw new IOException("Not enough money, negative balance is not possible");
+        }
+
+    }
 }
--- OptionMenu.java
+++ OptionMenu.java
@@ -12,11 +12,35 @@ public class OptionMenu extends Account {
     HashMap<Integer, Integer> data = new HashMap<Integer, Integer>();
 
     //Validates the login information
-    public void getLogin()
+    public void getLogin() throws IOException
     {
+        int x = 1; 
 
         data.put(123456789, 1234);
         data.put(987654321, 9876);
+
+        do {
+
+            try {
+
+                System.out.println("Welcome to the ATM-machine");
+
+                //Ask user to enter accountnumber and checks if the user have an account
+                System.out.print("Enter your account number: ");
+                SetCustomer(menuInput.nextInt());
+
+                //Ask user to enter their pin-code to confirm accsess to account
+                System.out.print("Enter your PIN-code");
+                SetPinCode(menuInput.nextInt());
+
+            } catch (Exception e) {
+                System.out.println("Invalid characters, only numbers" + "\n");
+                x = 2;  
+            }
+            //Checks the costumer has inputed correct information
+        } while(x == 1;)
+
+        
     }
 }
 
